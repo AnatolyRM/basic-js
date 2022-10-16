88bc0d52acf0bdfca0037437e62512583a3dbcff
AnatolyRM <1neytron@gmail.com>
2022-10-16 05:51:47 +0300

    feat: add dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,31 +6,29 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
 function dateSample(sampleActivity) {
-  const rateOfHalfPeriod = Math.log(2) / 5730;
-  if (
-      typeof sampleActivity !== 'string' ||
-      isNaN(sampleActivity) ||
-      Number(sampleActivity) > 15 ||
-      Number(sampleActivity) <= 0
-  ) {
-    return false;
-  }
+    if (typeof sampleActivity !== 'string' ||
+        !Number(sampleActivity) ||
+        Number(sampleActivity) > 15 ||
+        Number(sampleActivity) <= 0
+    ) {
+        return false
+    }
 
-  return Math.ceil(Math.log(15 / Number(sampleActivity)) / rateOfHalfPeriod);
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD))
 }
 
 module.exports = {
-  dateSample
+    dateSample
 };
