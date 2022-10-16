d9fa8e4d0aee31e632b191cd58883709320aab9b
AnatolyRM <1neytron@gmail.com>
2022-10-16 05:46:24 +0300

    feat: add countCats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const rateOfHalfPeriod = Math.log(2) / 5730;
+  if (
+      typeof sampleActivity !== 'string' ||
+      isNaN(sampleActivity) ||
+      Number(sampleActivity) > 15 ||
+      Number(sampleActivity) <= 0
+  ) {
+    return false;
+  }
+
+  return Math.ceil(Math.log(15 / Number(sampleActivity)) / rateOfHalfPeriod);
 }
 
 module.exports = {
