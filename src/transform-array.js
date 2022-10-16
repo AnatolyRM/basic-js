c73fcf90a4c19594f6672e5df64430dd2e2c30e5
AnatolyRM <1neytron@gmail.com>
2022-10-16 16:17:27 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+    const tempArr = []
+
+    if (!Array.isArray(arr)) {
+        throw new Error("'arr' parameter must be an instance of the Array!")
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i]) {
+                case '--double-prev':
+                    if (i > 0 && arr[i - 2] !== '--discard-next') tempArr.push(arr[i - 1])
+                    break
+                case '--double-next':
+                    if (i + 1 < arr.length) tempArr.push(arr[i + 1])
+                    break
+                case '--discard-next':
+                    i += 2
+                    break
+                case '--discard-prev':
+                    if (i > 0 && arr[i - 2] !== '--discard-next') tempArr.pop()
+                    break
+                default:
+                    tempArr.push(arr[i])
+                    break
+            }
+        }
+    }
+
+    return tempArr
 }
 
 module.exports = {
-  transform
+    transform
 };
